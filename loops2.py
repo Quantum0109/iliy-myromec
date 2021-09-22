52b2f7a4a71dbadb8e60550d77c60ae5c47b27c8
Quantum0109 <72807872+Quantum0109@users.noreply.github.com>
2021-09-22 17:38:52 +0300

    Add files via upload

--- loops2.py
+++ loops2.py
@@ -4,3 +4,9 @@ while user_answer not in ("1","2","3"):
 	user_answer = input("какая дорога")
 
 print("илья муромец поехал по", user_answer, "дорожке")
+if level == "011":
+	pass
+elif level == "012":
+	pass
+else:
+	pass

b0c14e70db9a5aeb315a7a45c657b14a79b27ede
Quantum0109 <72807872+Quantum0109@users.noreply.github.com>
2021-12-22 17:40:14 +0300

    Delete save_game.py

--- save_game.py
+++
@@ -1,9 +0,0 @@
-def save(user_name, user_money, user_hp, user_luck, user_inventory, game):
-    with open("save", "w", encoding="UTF-8") as file:
-        file.write(str(user_name) + "\n")
-        for item in user_inventory:
-            file.write(str(item) + "\n")
-        file.write(str(user_money) + "\n")
-        file.write(str(user_hp) + "\n")
-        file.write(str(user_luck) + "\n")
-        file.write(str(game) + "\n")

23cc4f4bfa93566e804828608c4025bed37907d8
Quantum0109 <72807872+Quantum0109@users.noreply.github.com>
2022-02-12 12:38:02 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,9 +1,18 @@
-def multi(number: int) -> int:
-    """
-        docstring
-    """
-    number *= 2
-    return number
+nums = range(101)
 
-result = multi(15.0)
-print(result)
+"""
+    на 3 - fizz
+    на 5 - bazz
+    на 3 и 5 - fizzbazz
+    остальные как есть
+"""
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("fizzbazz")
+    elif i % 5 == 0:
+        print("bazz")
+    elif i % 3 == 0:
+        print("fizz")
+    else: 
+        print(i)
